dc832ad97ff133adec362f7234415c2d38353fed
Waliul Islam Sumon <waliul@cse.kuet.ac.bd>
2025-10-05 02:58:34 +0600

    optimized the prime checker

--- prime checker.c
+++ prime checker.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-    int n = 1;
+    int n = 113;
     int cnt = 0;
 
-    if (n == 1)
+    if (n <= 1)
     {
         printf("%d is NOT prime", n);
         return 0;
     }
 
-    for (int i = 1; i <= n; i++)
-        if (n % i == 0)
-            cnt++;
+    for (int i = 2; i * i <= n; i++)
+            if (n % i == 0)
+                cnt++;
 
     if (cnt > 2)
         printf("%d is NOT prime", n);
